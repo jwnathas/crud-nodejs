ec63a1a2f4c48ff3a947d0acaf18577c0c3a8c32
Jonathas Silva <jonathasx57@gmail.com>
2023-10-05 13:07:04 -0300

    Funções do CRUD

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 const express = require('express')
-
 const server = express()
-
-//Query params = ?nome=NodeJS
-//Route params = /curso/2
-//requeste body = {nome: 'node js'}
 
 const cursos = ['node js', 'javascript', 'react native']
 
-//localhost:3000/cursos/2
-server.get('/curso/:index', (req, res) => {
+server.use(express.json())
 
+//retorna todos os cursos
+server.get('/cursos', (req, res) => {
+    return res.json(cursos)
+})
+
+//localhost:3000/cursos/2
+server.get('/cursos/:index', (req, res) => {
     const {index} = req.params
 
     return res.json(cursos[index])
 
+})
+
+//criando um novo curso
+server.post('/cursos', (req, res) => {
+    const {name} = req.body
+    cursos.push(name)
+
+    return res.json(cursos)
+})
+
+//atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+
+    const {index} = req.params
+    const {name} = req.body
+
+    cursos[index] = name
+    
+    return res.json(cursos)
+})
+
+//deletando algum curso
+
+server.delete('/cursos/:index', (req, res) => {
+
+    const {index} = req.params
+    
+    cursos.splice(index, 1)
+
+    return res.json(cursos)
 })
 
 server.listen(3000)
